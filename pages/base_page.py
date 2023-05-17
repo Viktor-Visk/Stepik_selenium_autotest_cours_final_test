8b3980b2755bbbd2665ee0c51b69f6394d0db6fb
Viktor Osmolovskiy <osmolovskiy.viktor@e-queo.com>
2023-05-17 14:16:48 +0300

    add base_page and main_page and update test_main_page for Page Object

---
+++ pages/base_page.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver import Remote as RemoteWebDriver
+
+class BasePage ():
+    def __init__(salf, browser: RemoteWebDriver, url):
+        self.browser = browser
+        self.url = url
+
+    def open(self):
+        self.browser.get(self.url)
+        
