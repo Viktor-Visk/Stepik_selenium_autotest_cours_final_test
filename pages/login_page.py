0d7a3c5b82ae7704ec8ee72822480942cfbd415e
Viktor Osmolovskiy <osmolovskiy.viktor@e-queo.com>
2023-05-19 09:46:45 +0300

    update and add test login page

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,6 +1,7 @@
 from .base_page import BasePage
 from .locators import LoginPageLocators
-from selenium.webdriver.common.by import By
+from .locators import MainPageLocators
+import time
 
 class LoginPage(BasePage):
     def should_be_login_page(self):
@@ -9,7 +10,8 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        assert self.browser.current_url == "http://selenium1py.pythonanywhere.com/ru/accounts/login/", "'Login' is not finded in url"
+        self.browser.find_element(*MainPageLocators.LOGIN_LINK).click()
+        assert  self.browser.current_url == "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/", "'Login' is not finded in url"
 
     def should_be_login_form(self):
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
--- test_main_page.py
+++ test_main_page.py
@@ -1,3 +1,4 @@
+import pytest
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 
@@ -15,7 +16,7 @@ def test_guest_should_see_login_link(browser):
     page.should_be_login_link()
 
 def test_should_be_login_url(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    link = "http://selenium1py.pythonanywhere.com/"
     page = LoginPage(browser, link)
     page.open()
     page.should_be_login_page()
